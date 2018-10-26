a9b98bd6b6ef8c8d830b22aad5ae6b494ca295b1
Pascal Jungblut <git@pascalj.de>
2018-10-26 17:03:46 +0200

    Pattern concept adaptions

--- patterns/include/patterns/BlockPattern.h
+++ patterns/include/patterns/BlockPattern.h
@@ -27,48 +27,99 @@ private:
 public:
   BlockPattern(size_t capacity) : _capacity(capacity) {}
 
-  template<typename Entity>
-  ValueType lbegin(const Entity &entity) {
+  template<typename Entity = DefaultEntity>
+  index_t lbegin(const Entity &entity = Entity::current()) {
     auto location = entity_mapping.map(entity);
     return ElementMapping::offset(entity, location, _capacity);
   }
 
-  template<typename Entity>
-  ValueType lend(const Entity &entity) {
+  template<typename Entity = DefaultEntity>
+  index_t lend(const Entity &entity = Entity::current()) {
     auto location = entity_mapping.map(entity);
     auto offset = ElementMapping::offset(entity, location, _capacity);
     auto size = ElementMapping::count(entity, location, _capacity);
     return offset + size;
   };
 
+  /**
+   * Maps the local coord to a local linear index
+   */
   index_t local_at(coord_t local_coord) {
-    return lbegin(DefaultEntity::current()) + local_coord;
+    return lbegin() + local_coord;
   }
-  index_t global_at(coord_t global_coord);
-  DefaultEntity unit_at(coord_t global_coord);
+
+  /**
+   * Maps a global coord to a global linear index
+   */
+  index_t global_at(coord_t global_coord) {
+    return global_coord;
+  }
+
+  /**
+   * Maps a global coordinate to its default entity
+   */
+  DefaultEntity unit_at(coord_t global_coord) {
+    
+  }
 
   // global to local
+  /**
+   * Maps a local coordinate to a global coordinate
+   */
   coord_t global(DefaultEntity, coord_t local_coord);
+
+  /**
+   * Maps a local coordinate to a global linear index
+   */
   index_t global_index(DefaultEntity, coord_t local_coord);
+
+  // TODO: dimensions
   /* coord_t global(EntityType, coord_t local_coord); */
   /* index_t global(EntityType, index_t local_index); */
   /* index_t global(index_t local_index); */
   //
   // blocks
+
+  /**
+   * Returns the number of blocks [in each dimension]
+   */
   size_t blockspec();
+
+  /**
+   * Maps a global coordinate to a block
+   */
   ViewSpec block_at(coord_t global_coord);
+
+  /**
+   * Maps the global linear inde to a block
+   */
   ViewSpec block(index_t global_index); 
+
+  /**
+   * Maps the local linear index to a block
+   */
   ViewSpec local_block(index_t local_index); 
+
+  /**
+   * Maps the local linear index to a local viewspec/offset
+   */
   ViewSpec local_block_local(index_t local_index); 
 
-  // locality test
-  bool is_local(index_t global_index, DefaultEntity);
-  // TODO Dimensions
+  /**
+   * Test whether the global index is local to the current unit
+   */
+  bool is_local(index_t global_index);
+  
+  /**
+   * Test whether the local index is local to the current unit
+   */
   /* bool is_local(dim_t dim, index_t local_index, EntityType); */
   
   size_t capacity();
   size_t local_capacity(DefaultEntity);
   size_t size();
+
+  template<typename EntityType>
   size_t local_size(EntityType);
   /* size_t extents(); */
   /* size_t extent(dim_t dimension); */
