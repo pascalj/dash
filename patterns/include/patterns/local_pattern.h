2171ac0528dff953e36d4c8309ed16d40846ef8b
Pascal Jungblut <pascal.jungblut@ifi.lmu.de>
2019-05-23 19:06:56 +0200

    Fix some bugs for mephisto

--- patterns/include/patterns/local_pattern.h
+++ patterns/include/patterns/local_pattern.h
@@ -71,14 +71,10 @@ public:
   BalancedLocalPattern() = delete;
 
   BalancedLocalPattern(Base base)
-    : Base(base)
-  {
-    for (auto blockcount : this->local_blockspec().extents()) {
-      _local_blocks *= blockcount;
-    }
-  }
+    : Base(base), _local_blocks(base.local_blockspec().size())
+  { }
 
-  FN_HOST_ACC auto block_for_entity(Entity entity, size_t index) const
+  auto block_for_entity(Entity entity, size_t index) const
   {
     auto block_index =
         (std::ceil(static_cast<float>(_local_blocks) / entity.total()) *
@@ -87,7 +83,7 @@ public:
     return this->local_block(block_index);
   }
 
-  FN_HOST_ACC auto block_local_for_entity(Entity entity, size_t index) const
+  auto block_local_for_entity(Entity entity, size_t index) const
   {
     auto block_index =
         (std::ceil(static_cast<float>(_local_blocks) / entity.total()) *
@@ -96,33 +92,17 @@ public:
     return this->local_block_local(block_index);
   }
 
-  FN_HOST_ACC block_range blocks_local_for_entity(Entity &entity)
+  block_range blocks_local_for_entity(const Entity &entity) const
   {
     return block_range{this, &entity};
   }
 
-  FN_HOST_ACC size_t nblocks_for_entity(Entity entity) const
+  size_t nblocks_for_entity(Entity entity) const
   {
     auto const entity_blocks = _local_blocks / entity.total();
     auto const add =
         ((_local_blocks % entity.total()) <= entity.index()) ? 0 : 1;
     return entity_blocks + add;
-  }
-
-  size_t lbegin(Entity entity)
-  {
-    auto first_blockspec = this->block_local_for_entity(entity, 0);
-    DASH_LOG_TRACE(
-        "BalancedLocalPattern.lbegin", "first_blockspec", first_blockspec);
-    return this->local_at(first_blockspec.offsets());
-  }
-
-  size_t lend(Entity entity)
-  {
-    auto total_local_blocks = nblocks_for_entity(entity);
-    auto last_blockspec =
-        this->block_local_for_entity(entity, total_local_blocks - 1);
-    return this->local_at(last_blockspec.offsets()) + last_blockspec.size();
   }
 
 private:
